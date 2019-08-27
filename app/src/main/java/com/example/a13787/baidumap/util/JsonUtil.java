0670ec1c87b1be00219d47e7c2eca2c066dfddf0
chenyuqi0215 <10175101214@stu.ecnu.edu.cn>
2019-08-28 00:28:21 +0800

    fastjson

--- app/src/main/java/com/example/a13787/baidumap/util/JsonUtil.java
+++ app/src/main/java/com/example/a13787/baidumap/util/JsonUtil.java
@@ -2,6 +2,7 @@ package com.example.a13787.baidumap.util;
 
 import com.alibaba.fastjson.JSON;
 
+import com.alibaba.fastjson.TypeReference;
 import com.example.a13787.baidumap.entity.UserEntity;
 
 
@@ -20,97 +21,23 @@ public class JsonUtil
     {
         String jsonStr = com.alibaba.fastjson.JSONObject.toJSONString(userEntity);
         return jsonStr;
-
-        /*JSONObject jsonObject = new JSONObject();
-        try
-        {
-            jsonObject.put("age",userEntity.getAge());
-            jsonObject.put("birth",userEntity.getBirth());
-            jsonObject.put("department",userEntity.getDepartment());
-            jsonObject.put("email",userEntity.getEmail());
-            jsonObject.put("name",userEntity.getName());
-            jsonObject.put("nickname",userEntity.getNickname());
-            jsonObject.put("password",userEntity.getPassword());
-            jsonObject.put("phone",userEntity.getPhone());
-            jsonObject.put("portraiturl",userEntity.getPortraiturl());
-            jsonObject.put("school",userEntity.getSchool());
-            jsonObject.put("schoolid",userEntity.getSchoolid());
-            jsonObject.put("sex",userEntity.getSex());
-            jsonObject.put("userid",userEntity.getUserid());
-        }
-        catch (Exception e)
-        {
-            return null;
-        }
-        finally
-        {
-            return jsonObject.toString();
-        }*/
     }
 
     public static UserEntity jsonToUser(JSONObject jsonObject)
     {
         UserEntity userEntity = JSON.parseObject(JSON.toJSONString(jsonObject),UserEntity.class);
         return userEntity;
-        /*try
-        {
-            userEntity.setAge(jsonObject.getInt("age"));
-            userEntity.setBirth(jsonObject.getString("birth"));
-            userEntity.setDepartment(jsonObject.getString("department"));
-            userEntity.setEmail(jsonObject.getString("email"));
-            userEntity.setName(jsonObject.getString("name"));
-            userEntity.setNickname(jsonObject.getString("nickname"));
-            userEntity.setPassword(jsonObject.getString("password"));
-            userEntity.setPhone(jsonObject.getString("phone"));
-            userEntity.setPortraiturl(jsonObject.getString("portraiturl"));
-            userEntity.setSchool(jsonObject.getString("school"));
-            userEntity.setSchoolid(jsonObject.getString("schoolid"));
-            userEntity.setSex(jsonObject.getString("sex"));
-            userEntity.setUserid(jsonObject.getInt("userid"));
-        }
-        catch (Exception e)
-        {
-            return null;
-        }
-        finally
-        {
-            return userEntity;
-        }*/
     }
 
     public static ArrayList<UserEntity> jsonToUsers(String data)
     {
-        ArrayList<UserEntity> ret = new ArrayList<>();
-        try
-        {
-            JSONArray jsonArray = new JSONArray(data);
-            for( int i=0; i<jsonArray.length(); i++ )
-            {
-                JSONObject jsonObject = jsonArray.getJSONObject(i);
-                UserEntity userEntity = jsonToUser(jsonObject);
-                ret.add(userEntity);
-            }
-        }
-        catch (Exception e)
-        {
-            return null;
-        }
-        finally
-        {
-            return ret;
-        }
+        ArrayList<UserEntity> userEntities = JSON.parseObject(data, new TypeReference<ArrayList<UserEntity>>() {});
+        return userEntities;
     }
 
     public static UserEntity jsonToUser(String data)
     {
-        try
-        {
-            JSONObject jsonObject = new JSONObject(data);
-            return jsonToUser(jsonObject);
-        }
-        catch (Exception e)
-        {
-            return null;
-        }
+        UserEntity userEntity = JSON.parseObject(data, new TypeReference<UserEntity>() {});
+        return userEntity;
     }
 }
